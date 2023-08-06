fa36e8bd7e7969b161486c534a9eb7ca30122aa2
nivedita9009 <nsoni7361@gmail.com>
2023-08-06 23:49:23 +0530

    goggle task

--- src/App.js
+++ src/App.js
@@ -1,112 +1,90 @@
-import React, { useState,useRef } from 'react'
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer  } from '@react-google-maps/api'
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  IconButton,
-  SkeletonText,
-} from '@chakra-ui/react'
-import { FaTimes } from 'react-icons/fa'
+import './App.css';
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete,DirectionsRenderer} from '@react-google-maps/api';
+import { useRef, useState } from 'react';
 
-const center = { lat: 48.8584, lng: 2.2945 }
+const center = { lat: 23.16914528880484, lng: 77.41922136083194 }
 
 function App() {
+  const [map, setMap] = useState(/**@type google.maps.map */(null))
+  const[directionsResponse,setDirectionResponse]=useState(null)
+  const[distance,setDistance]=useState('')
+
+  const originRef=useRef()
+  const destinationRef=useRef()
+
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "", ///GoogleApi key
+    googleMapsApiKey: "", ///googleApikey 
     libraries: ['places']
   })
-
-  const [map, setMap] = useState( /** @type google.maps.Map */(null))
-  const [direction, setDirection] = useState(null)
-  const [distance, setDistance] = useState("")
-
-  const originRef = useRef()
-  const destinationRef = useRef()
-
   if (!isLoaded) {
-    return <SkeletonText />
-  }
+    return <h1>Loading....</h1>
 
-  async function calculateRoute() {
-    if (originRef.current.value === '' || destinationRef.current.value === '') {
+  }
+  async function calculateRoute(){
+    if(originRef.current.value==='' || destinationRef.current.value===''){
       return
     }
-      //es
+    //es
     // eslint-disable-next-line no-undef
-    const directionService = new google.maps.DirectionsService()
-    const results = await directionService.route({
-      origin: originRef.current.value,
-      destination: destinationRef.current.value,
+    const directionService=new google.maps.DirectionsService()
+    const results=await directionService.route({
+      origin:originRef.current.value,
+      destination:destinationRef.current.value,
       
       // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING
+      travelMode:google.maps.TravelMode.DRIVING
     })
-    setDirection(results)
-    setDistance(results.routes[0].legs[0].distance.text)
+    setDirectionResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text);
   }
-
-  function clearRoute() {
-    setDirection(null)
-    setDistance('')
-    originRef.current.value = ''
-    destinationRef.current.value = ''
+  function clearRoute(){
+   window.location.reload();
+    
+   
   }
 
   return (
-    <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      backgroundPosition='bottom'
-      height='100vh'
-      width='100vw'
-    >
-      <Box position='absolute' left={0} top={0} height='100%' width='100%'>
-        <GoogleMap center={center} zoom={15} mapContainerStyle={{ width: '100%', height: '100%' }}
-          onLoad={(map) => setMap(map)} >
-          <Marker position={center} />
-          {direction && <DirectionsRenderer directions={direction} />}
+    <div className="App" style={{ display: "flex", flexDirection: 'column', gap: '20px', justifyContent: "center", alignItems: 'center', height: "100vh" }}>
+      <div style={{ display: "flex", gap: '10px' }}>
+        <div>
+          <label>Origin</label>
+          <Autocomplete>
+            <input type='text' ref={originRef} />
+          </Autocomplete>
+
+        </div>
+        <div>
+          <label>Destination</label>
+          <Autocomplete>
+            <input type='text' ref={destinationRef} />
+          </Autocomplete>
+
+
+        </div>
+        <div>
+          <button onClick={()=>calculateRoute()} >GO</button>
+        </div>
+        <div>
+          <button onClick={() => clearRoute()}>Reset</button>
+        </div>
+      </div>
+      <h3>Distance:{distance}</h3>
+      {/* ////////MAP//////// */}
+      <div style={{ height: '500px', width: "500px", backgroundColor: 'red' }} >
+        <GoogleMap center={center} zoom={4} mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            fullscreenControl: false
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+         
+          {
+            directionsResponse && <DirectionsRenderer directions={directionsResponse}/>
+          }
         </GoogleMap>
-      </Box>
-
-      <Box
-        // p={4}
-        // borderRadius='lg'
-        // mt={4}
-        // backgroundColor='white'
-        // shadow='base'
-        minWidth='container.md'
-        zIndex='modal'
-      >
-
-        <Autocomplete>
-          <input type='text' placeholder='Origin' ref={originRef} />
-        </Autocomplete>
-        <Autocomplete>
-          <input type='text' placeholder='Destination' ref={destinationRef} />
-        </Autocomplete>
-
-        <ButtonGroup>
-          <Button colorScheme='blue' type='submit' onClick={calculateRoute}>
-            Magic
-          </Button>
-          <IconButton
-            aria-label='center back'
-            icon={<FaTimes />}
-            onClick={clearRoute}
-          />
-        </ButtonGroup>
-
-      </Box>
-    </Flex>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
-
